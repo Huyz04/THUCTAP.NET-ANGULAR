8fb73f5efa9d34f0f916596b8ec11d19102e5b54
Ngocquyentran <quyentrthng@gmail.com>
2024-07-04 15:21:25 +0700

    test ctrller

---
+++ INTERN.Tests/TestControllers/TestProductController_Delete.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using INTERN.Controllers;
+using INTERN.DTO;
+using INTERN.Model;
+using INTERN.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Type = INTERN.Model.Type;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace INTERN.Tests.TestControllers
+{
+    public class TestProductController_Delete
+    {
+        private readonly ProductContext _context;
+        private readonly IMapper _mapper;
+        private readonly PProduct _pProduct;
+        private readonly ProductController _productController;
+        public TestProductController_Delete()
+        {
+            // Setup InMemory database
+            var options = new DbContextOptionsBuilder<ProductContext>()
+            .UseInMemoryDatabase(databaseName: "ProductDatabase")
+            .Options;
+            _context = new ProductContext(options);
+
+            // Seed data
+            var type1 = new Type
+            {
+                NameType = "Type1",
+                Created_at = DateTime.Now,
+                Created_by = "1",
+                Updated_at = DateTime.Now,
+                Updated_by = "1"
+            };
+            var product1 = new Product
+            {
+                Code = "P01",
+                Name = "Product1",
+                Type = type1,
+                Description = "string",
+                Price = 100,
+                Created_at = DateTime.Now,
+                Created_by = "1",
+                Updated_at = DateTime.Now,
+                Updated_by = "1"
+            };
+            _context.Products.Add(product1);
+            _context.SaveChanges();
+
+            // Setup AutoMapper
+            _mapper = A.Fake<IMapper>();
+
+            // Initialize PProduct with the real context and mapper
+            _pProduct = new PProduct(_context, _mapper);
+            _productController = new ProductController(_context, _pProduct,_mapper);
+        }
+        [Fact]
+        public async Task DeleteProduct_Successfully()
+        {
+
+            // Act
+            var result = await _productController.DeleteProduct(1);
+
+            // Assert
+            //result.Should().BeNull();
+            var actionResult = Assert.IsType<ActionResult<Response>>(result);
+            var response = Assert.IsType<Response>(actionResult.Value);
+            Assert.True(response.Success);
+            Assert.Equal("Success!", response.Message);
+        }
+
+        [Fact]
+        public async Task DeleteProduct_InvalidID_Failed()
+        {
+
+            // Act
+            var result = await _productController.DeleteProduct(-1);
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<Response>>(result);
+            var response = Assert.IsType<Response>(actionResult.Value);
+            Assert.False(response.Success);
+            Assert.Equal("Invalid ID!", response.Message);
+        }
+    }
+}
